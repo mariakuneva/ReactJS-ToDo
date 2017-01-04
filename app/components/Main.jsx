5566f8e93e4040d04df061d5d55f5f0f1bc12140
Maria <mariakunevaa@gmail.com>
2017-01-04 15:38:31 +0200

    Project init

---
+++ app/components/Main.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React Hy</h2>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default Main;
